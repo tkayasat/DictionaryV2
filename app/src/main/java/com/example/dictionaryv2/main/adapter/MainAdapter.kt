c48335320f187b73cbc108048162f58cff3baddb
tkaya <tkayasat@gmail.com>
2022-02-10 17:24:09 +0300

    hw 6 не рабоатет изза ошибки - groovy.lang.MissingPropertyException: Could not get unknown property 'Config' for extension 'android' of type com.android.build.gradle.internal.dsl.BaseAppModuleExtension.-  Caused by: groovy.lang.MissingPropertyException: Could not get unknown property 'Config' for extension 'android' of type com.android.build.gradle.internal.dsl.BaseAppModuleExtension.
    hw 7 done - не проверить изза ошибки в 6

---
+++ app/src/main/java/com/example/dictionaryv2/main/adapter/MainAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.dictionaryv2.main.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dictionaryv2.R
+import com.example.dictionaryv2.utils.convertMeaningsToSingleString
+
+class MainAdapter(private var onListItemClickListener: OnListItemClickListener) :
+    RecyclerView.Adapter<MainAdapter.RecyclerItemViewHolder>() {
+
+    private var data: List<DataModel> = arrayListOf()
+
+    fun setData(data: List<DataModel>) {
+        this.data = data
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerItemViewHolder {
+        return RecyclerItemViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.activity_main_recyclerview_item, parent, false) as View
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerItemViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    inner class RecyclerItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        fun bind(data: DataModel) {
+            if (layoutPosition != RecyclerView.NO_POSITION) {
+                itemView.findViewById<TextView>(R.id.header_textview_recycler_item).text = data.text
+                itemView.findViewById<TextView>(R.id.description_textview_recycler_item).text =
+                    convertMeaningsToSingleString(data.meanings)
+                itemView.setOnClickListener { openInNewWindow(data) }
+            }
+        }
+    }
+
+    private fun openInNewWindow(listItemData: DataModel) {
+        onListItemClickListener.onItemClick(listItemData)
+    }
+
+    interface OnListItemClickListener {
+        fun onItemClick(data: DataModel)
+    }
+}
---
+++ app/src/main/java/com/example/dictionaryv2/utils/SearchResultParser.kt
@@ -0,0 +1,88 @@
+package com.example.dictionaryv2.utils
+
+fun mapSearchResultToResult(searchResults: List<SearchResultDto>): List<DataModel> {
+    return searchResults.map { searchResult ->
+        var meanings: List<Meaning> = listOf()
+        searchResult.meanings?.let {
+
+            meanings = it.map { meaningsDto ->
+                Meaning(
+                    TranslatedMeaning(meaningsDto?.translation?.translation ?: ""),
+                    meaningsDto?.imageUrl ?: ""
+                )
+            }
+        }
+        DataModel(searchResult.text ?: "", meanings)
+    }
+}
+
+fun parseOnlineSearchResults(data: AppState): AppState {
+    return AppState.Success(mapResult(data, true))
+}
+
+private fun mapResult(
+    data: AppState,
+    isOnline: Boolean
+): List<DataModel> {
+    val newSearchResults = arrayListOf<DataModel>()
+    when (data) {
+        is AppState.Success -> {
+            getSuccessResultData(data, isOnline, newSearchResults)
+        }
+    }
+    return newSearchResults
+}
+
+private fun getSuccessResultData(
+    data: AppState.Success,
+    isOnline: Boolean,
+    newSearchDataModels: ArrayList<DataModel>
+) {
+    val searchDataModels: List<DataModel> = data.data as List<DataModel>
+    if (searchDataModels.isNotEmpty()) {
+        if (isOnline) {
+            for (searchResult in searchDataModels) {
+                parseOnlineResult(searchResult, newSearchDataModels)
+            }
+        } else {
+            for (searchResult in searchDataModels) {
+                newSearchDataModels.add(
+                    DataModel(
+                        searchResult.text,
+                        arrayListOf()
+                    )
+                )
+            }
+        }
+    }
+}
+
+private fun parseOnlineResult(
+    searchDataModel: DataModel,
+    newSearchDataModels: ArrayList<DataModel>
+) {
+    if (searchDataModel.text.isNotBlank() && searchDataModel.meanings.isNotEmpty()) {
+        val newMeanings = arrayListOf<Meaning>()
+        newMeanings.addAll(searchDataModel.meanings.filter { it.translatedMeaning.translatedMeaning.isNotBlank() })
+        if (newMeanings.isNotEmpty()) {
+            newSearchDataModels.add(
+                DataModel(
+                    searchDataModel.text,
+                    newMeanings
+                )
+            )
+        }
+    }
+}
+
+fun convertMeaningsToSingleString(meanings: List<Meaning>): String {
+    var meaningsSeparatedByComma = String()
+    for ((index, meaning) in meanings.withIndex()) {
+        meaningsSeparatedByComma += if (index + 1 != meanings.size) {
+            String.format("%s%s", meaning.translatedMeaning.translatedMeaning, ", ")
+        } else {
+            meaning.translatedMeaning.translatedMeaning
+        }
+    }
+    return meaningsSeparatedByComma
+}
